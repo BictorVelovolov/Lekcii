6693ced2c07314027d59bd795bdfb9f415a7eebd
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2022-11-28 14:35:10 +0300

    Update ertyuillkjnbvcvit.cpp

--- ertyuillkjnbvcvit.cpp
+++ ertyuillkjnbvcvit.cpp
@@ -4,22 +4,25 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
+    int a, b, ch1, ch2, ch3, summ;
     string l;
     ifstream file;
+    ofstream file2;
     file.open("aboba.txt");
+    file2.open("KalAboby.txt");
     file >> a;
     cout << a << " ";
     file >> b;
-    cout << b << " ";;
+    cout << b << " ";
+    summ = 0;
    while (a > 0)
    {
        a--;
-       getline (file, l);
-       cout << l << endl;
+       file >> ch1 >> ch2 >> ch3;
+       summ = summ + ch1;
    }
+   file2 << summ;
+   file2.close();
     file.close();
-
-
     return 0;
 }
